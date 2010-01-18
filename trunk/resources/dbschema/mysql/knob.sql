9542ce2818330c4460ccc1ce6a615698e33ddae2
btnguyen2k@gmail.com <btnguyen2k@gmail.com@229a8632-00fe-11df-9289-67a056c6595c>
2010-01-18 14:16:59 +0000

    * SQL Schema: knob_app_config table.

--- trunk/resources/dbschema/mysql/knob.sql
+++ trunk/resources/dbschema/mysql/knob.sql
@@ -1,4 +1,21 @@
-#Database schema for Knob
-#Database type: MySQL 5.x
+# Database schema for Knob
+# Database type: MySQL 5.x
 
-#Note: for MySQL, please use DELETE TABLE IF EXISTS and CREATE TABLE IF NOT EXISTS whenever possible!
+# Note: for MySQL, please use DELETE TABLE IF EXISTS and CREATE TABLE IF NOT EXISTS whenever possible!
+
+# Drop existing tables
+DROP TABLE IF EXISTS knob_app_config;
+
+# Create tables
+CREATE TABLE IF NOT EXISTS knob_app_config (
+	config_domain					VARCHAR(32),
+	config_key						VARCHAR(64),
+	config_long						BIGINT,
+	config_double					DOUBLE,
+	config_string					MEDIUMTEXT,
+	config_boolean					CHAR(1),
+	config_date						TIMESTAMP,
+	config_binary					MEDIUMBLOB,
+	INDEX (config_key),
+	PRIMARY KEY (config_domain, config_key)
+) Engine = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci;
